255641a932f1bacca828e0cf950e4bbb5974f0a9
soHyn <sohyn2208@gmail.com>
2024-02-22 21:19:53 +0900

    refactor: 2_add_page.dart UI 수정

--- lib/screens/2_add_page.dart
+++ lib/screens/2_add_page.dart
@@ -1,8 +1,5 @@
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 
 // 2.1 제품 등록 페이지
 class AddPage extends StatefulWidget {
@@ -19,7 +16,7 @@ class AddState extends State<AddPage> {
 
     return Scaffold(
       // 전체화면 색상
-      backgroundColor: Color(0xFFF5F5F5),
+      backgroundColor: const Color(0xFFF5F5F5),
 
       body: SafeArea(
         child: Column(
@@ -27,75 +24,77 @@ class AddState extends State<AddPage> {
           children: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Add Your Product',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-            ),
-            /**
-             * URL 입력란
-             */
-            SizedBox(height: 10.0,),
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              width: screenWidth,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  const Text("URL",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.white,
-                  ),),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFFFFFFF),
-                            borderRadius: BorderRadius.circular(8),
-                            // boxShadow: [
-                            //   BoxShadow(
-                            //     color: Colors.grey,
-                            //     offset: Offset(0, 10),
-                            //     blurRadius: 4.0,
-                            //
-                            //   ),
-                            //   BoxShadow(
-                            //       color: Colors.grey,
-                            //       offset: Offset(0, 10),
-                            //       blurRadius: 4.0
-                            //   ),
-                            // ]
-                          ),
-                          child: TextField(
-                            // controller: _textController,
-                            decoration: InputDecoration(
-                              labelText: 'URL을 입력하세요',
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                                borderSide: BorderSide.none,
+                children: [
+                  const Text(
+                    'Add Your Product',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  /**
+                   * URL 입력박스
+                   */
+                  const SizedBox(height: 45.0,),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                    padding: const EdgeInsets.fromLTRB(12, 20, 5, 5),
+                    width: screenWidth,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const Text("URL",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: const Color(0xFFFFFFFF),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      offset: Offset(0, 5),
+                                      blurRadius: 10,
+                                      color: Colors.grey,
+                                      inset: true,
+                                    ),
+                                  ],
+                                ),
+                                child: TextField(
+                                  // controller: _textController,
+                                  decoration: InputDecoration(
+                                    labelText: 'URL을 입력하세요',
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      borderSide: BorderSide.none,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
-                          ),
+                            const SizedBox(width: 5.0),
+                            IconButton(
+                              iconSize: 50.0,
+                              icon: const Icon(Icons.arrow_circle_right),
+                              onPressed: () { print("onPressed!"); },
+                            ),
+                          ],
                         ),
-                      ),
-                      const SizedBox(width: 16.0),
-                      IconButton(
-                        // iconSize: ,
-                          icon: const Icon(Icons.arrow_circle_right),
-                          onPressed: () {print("onPressed!");},
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ],
               ),
