1f8657928f9e1280ac93308f0a8b96eef7a72ab8
Glenn-Bonsai <xeojine@gmail.com>
2024-02-25 18:42:40 +0900

    feat: info_archive_page에서 radio button 추가.

--- lib/components/archive_info_box.dart
+++ lib/components/archive_info_box.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
-class TextBox_2 extends StatelessWidget{
+class TextBox_2 extends StatefulWidget {
   final String instruction;
 
-  const TextBox_2({super.key, required this.instruction});
+  const TextBox_2({Key? key, required this.instruction}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  _TextBox_2State createState() => _TextBox_2State();
+}
+
+class _TextBox_2State extends State<TextBox_2> {
+  String? _selectedOption = 'Select one option!';
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(25.0),
       child: Container(
         width: 300,
-        height: 300,
+        height: 500,
         decoration: ShapeDecoration(
             color: Colors.white,
             shape: RoundedRectangleBorder(
@@ -23,27 +30,70 @@ class TextBox_2 extends StatelessWidget{
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                'Disposal Guidance',
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w800,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Disposal Guidance',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w800,
+                  ),
                 ),
               ),
 
               SizedBox(height: 10),
 
               // radio button(Upcycling & Disposal)
+              Row(
+                children: [
+                  Radio<String>(
+                    value: 'Upcycling',
+                    groupValue: _selectedOption,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedOption = value;
+                      });
+                    },
+                  ),
+                  const Text('Upcycling',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Radio<String>(
+                    value: 'Disposal',
+                    groupValue: _selectedOption,
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedOption = value;
+                      });
+                    },
+                  ),
+                  const Text('Disposal',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
 
               SizedBox(height: 10),
 
-              Text(instruction,
-                //textAlign: TextAlign.start,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 14,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(widget.instruction,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  ),
                 ),
               ),
             ],
